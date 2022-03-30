e2c90f7716766de115d6baf908d03b24aeb0bb11
JaydenDev <willsjayden2@gmail.com>
2022-03-30 16:22:47 -0500

    working!

--- global.js
+++ global.js
@@ -63,4 +63,4 @@ const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
 const user = urlParams.get('user');
-document.querySelector('#input').value = user;
+document.getElementById('input').value = user;
